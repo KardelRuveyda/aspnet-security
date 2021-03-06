d2a692397549b3b07e4b544b35b4ef0396ad5136
KardelRuveyda <ruveydakardelcetin@gmail.com>
2021-03-06 16:33:55 +0300

    I transport Sql Connection Product Controllers to Startup.cs

--- DataProtection.Web/Models/NetCoreSecurityContext.cs
+++ DataProtection.Web/Models/NetCoreSecurityContext.cs
@@ -17,14 +17,6 @@ namespace DataProtection.Web.Models
 
         public virtual DbSet<Product> Product { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-HN6O7DQ\\SQLEXPRESS;Initial Catalog=NetCoreSecurity;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- DataProtection.Web/Startup.cs
+++ DataProtection.Web/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataProtection.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,10 @@ namespace DataProtection.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NetCoreSecurityContext>(options =>
+            {
+                options.UseSqlServer("Data Source=DESKTOP-HN6O7DQ\\SQLEXPRESS;Initial Catalog=NetCoreSecurity;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            });
             services.AddControllersWithViews();
         }
 
