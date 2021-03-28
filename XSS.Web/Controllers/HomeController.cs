3f12fe324584bf3897bf1bbbd4f1066f9b6b1ba5
KardelRuveyda <ruveydakardelcetin@gmail.com>
2021-03-29 01:03:47 +0300

    XSS

---
+++ XSS.Web/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using XSS.Web.Models;
+
+namespace XSS.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult CommentAdd()
+        {
+            HttpContext.Response.Cookies.Append("email", "kardeltest@gmail.com");
+            HttpContext.Response.Cookies.Append("password", "1234");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CommentAdd(string name,string comment)
+        {
+            ViewBag.name = name;
+            ViewBag.comment = comment;
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
